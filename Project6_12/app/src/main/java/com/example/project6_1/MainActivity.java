1a33eef77d9048dff4cfbee341f6129db6d87f64
“깃 <hy04730@naver.com>
2021-10-16 16:52:59 +0900

    Project6_1(과제)

---
+++ Project6_12/app/src/main/java/com/example/project6_1/MainActivity.java
@@ -0,0 +1,129 @@
+package com.example.project6_1;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.view.View;
+import android.widget.AutoCompleteTextView;
+import android.widget.CalendarView;
+import android.widget.Chronometer;
+import android.widget.DatePicker;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity {
+    Chronometer chronometer;
+    RadioButton rdoCal, rdoTime;
+    DatePicker dPicker;
+    TimePicker tPicker;
+    TextView tvYear, tvYear2, tvMonth, tvDay, tvHour, tvMinute;
+    AutoCompleteTextView autoComplete;
+    int selectYear, selectMonth, selectDay;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("시간 예약(수정)");
+
+        chronometer = (Chronometer)findViewById(R.id.chronometer);
+        rdoCal = (RadioButton)findViewById(R.id.rdoCal);
+        rdoTime = (RadioButton)findViewById(R.id.rdoTime);
+        tPicker = (TimePicker)findViewById(R.id.tPicker);
+        dPicker = (DatePicker)findViewById(R.id.dPicker);
+        tvYear = (TextView)findViewById(R.id.tvYear);
+        tvYear2 = (TextView)findViewById(R.id.tvYear2);
+        tvMonth = (TextView)findViewById(R.id.tvMonth);
+        tvDay = (TextView)findViewById(R.id.tvDay);
+        tvHour = (TextView)findViewById(R.id.tvHour);
+        tvMinute = (TextView)findViewById(R.id.tvMinute);
+        autoComplete = (AutoCompleteTextView)findViewById(R.id.autoComplete);
+
+        tPicker.setVisibility(View.INVISIBLE);
+        dPicker.setVisibility(View.INVISIBLE);
+        rdoCal.setVisibility(View.INVISIBLE);
+        rdoTime.setVisibility(View.INVISIBLE);
+
+        rdoCal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tPicker.setVisibility(View.INVISIBLE);
+                dPicker.setVisibility(View.VISIBLE);
+            }
+        });
+        rdoTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tPicker.setVisibility(View.VISIBLE);
+                dPicker.setVisibility(View.INVISIBLE);
+            }
+        });
+
+        chronometer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chronometer.setBase(SystemClock.elapsedRealtime());
+                chronometer.start();
+                chronometer.setTextColor(Color.RED);
+                rdoCal.setVisibility(View.VISIBLE);
+                rdoTime.setVisibility(View.VISIBLE);
+            }
+        });
+
+        tvYear.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                chronometer.stop();
+                chronometer.setTextColor(Color.BLUE);
+
+                tvYear.setText(Integer.toString(selectYear));
+                tvMonth.setText(Integer.toString(selectMonth));
+                tvDay.setText(Integer.toString(selectDay));
+                tvHour.setText(Integer.toString(tPicker.getCurrentHour()));
+                tvMinute.setText(Integer.toString(tPicker.getCurrentMinute()));
+
+                rdoCal.setVisibility(View.INVISIBLE);
+                rdoTime.setVisibility(View.INVISIBLE);
+                dPicker.setVisibility(View.INVISIBLE);
+                tPicker.setVisibility(View.INVISIBLE);
+                return false;
+            }
+        });
+        tvYear2.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                chronometer.stop();
+                chronometer.setTextColor(Color.BLUE);
+
+                tvYear.setText(Integer.toString(selectYear));
+                tvMonth.setText(Integer.toString(selectMonth));
+                tvDay.setText(Integer.toString(selectDay));
+                tvHour.setText(Integer.toString(tPicker.getCurrentHour()));
+                tvMinute.setText(Integer.toString(tPicker.getCurrentMinute()));
+
+                rdoCal.setVisibility(View.INVISIBLE);
+                rdoTime.setVisibility(View.INVISIBLE);
+                dPicker.setVisibility(View.INVISIBLE);
+                tPicker.setVisibility(View.INVISIBLE);
+                return false;
+            }
+        });
+
+
+        dPicker.setOnDateChangedListener(new DatePicker.OnDateChangedListener() {
+            @Override
+            public void onDateChanged(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                selectYear = year;
+                selectMonth = monthOfYear + 1;
+                selectDay = dayOfMonth;
+            }
+        });
+
+    }
+}
