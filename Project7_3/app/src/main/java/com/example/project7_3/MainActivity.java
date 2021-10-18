c8e56887201bda53693f8a40cf7789784f6b3e3d
“깃 <hy04730@naver.com>
2021-10-18 11:49:32 +0900

    Project7_3(2)

--- Project7_3/app/src/main/java/com/example/project7_3/MainActivity.java
+++ Project7_3/app/src/main/java/com/example/project7_3/MainActivity.java
@@ -5,7 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.Display;
+import android.view.Gravity;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -14,9 +17,8 @@ import android.widget.Toast;
 import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
-    TextView tvName, tvEmail;
     Button button1;
-    EditText dlgEdtName, dlgEdtEmail;
+    EditText edtName, edtEmail, dlgEdt1, dlgEdt2;
     TextView toastText;
     View dialogView, toastView;
 
@@ -26,14 +28,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         setTitle("사용자 정보 입력");
 
-        tvName = (TextView) findViewById(R.id.tvName);
-        tvEmail = (TextView) findViewById(R.id.tvEmail);
+        edtName = (EditText)findViewById(R.id.edtName);
+        edtEmail = (EditText)findViewById(R.id.edtEmail);
         button1 = (Button) findViewById(R.id.button1);
 
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 dialogView = (View)View.inflate(MainActivity.this, R.layout.dialog, null);
+                dlgEdt1 = (EditText) dialogView.findViewById(R.id.dlgEdt1);
+                dlgEdt2 = (EditText) dialogView.findViewById(R.id.dlgEdt2);
+
+                dlgEdt1.setText(edtName.getText().toString());
+                dlgEdt2.setText(edtEmail.getText().toString());
+
                 AlertDialog.Builder dlg = new AlertDialog.Builder(MainActivity.this);
                 dlg.setTitle("사용자 정보 입력");
                 dlg.setIcon(R.drawable.ic_launcher_foreground);
@@ -42,11 +50,8 @@ public class MainActivity extends AppCompatActivity {
                         new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
-                                dlgEdtName = (EditText)dialogView.findViewById(R.id.dlgEdt1);
-                                dlgEdtEmail = (EditText)dialogView.findViewById(R.id.dlgEdt2);
-
-                                tvName.setText(dlgEdtName.getText().toString());
-                                tvEmail.setText(dlgEdtEmail.getText().toString());
+                                edtName.setText(dlgEdt1.getText().toString());
+                                edtEmail.setText(dlgEdt2.getText().toString());
                             }
                         });
                 dlg.setNegativeButton("취소",
@@ -58,6 +63,12 @@ public class MainActivity extends AppCompatActivity {
                                 toastText = (TextView)toastView.findViewById(R.id.toastText1);
                                 toastText.setText("취소했습니다.");
                                 toast.setView(toastView);
+
+                                Display display = ((WindowManager) getSystemService(WINDOW_SERVICE)).getDefaultDisplay();
+                                int xOffset = (int)(Math.random()*display.getWidth());
+                                int yOffset = (int)(Math.random()*display.getHeight());
+
+                                toast.setGravity(Gravity.TOP | Gravity.LEFT, xOffset, yOffset);
                                 toast.show();
                             }
                         });
