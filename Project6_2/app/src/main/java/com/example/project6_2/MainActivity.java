0aca32c424bb9cfc8e361863016829ff8e589da2
“깃 <hy04730@naver.com>
2021-10-22 11:00:51 +0900

    ViewFlipper(1)

--- Project6_2/app/src/main/java/com/example/project6_2/MainActivity.java
+++ Project6_2/app/src/main/java/com/example/project6_2/MainActivity.java
@@ -3,12 +3,35 @@ package com.example.project6_2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ViewFlipper;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnPrev, btnNext;
+    ViewFlipper viewFlipper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnPrev = (Button)findViewById(R.id.btnPrev);
+        btnNext = (Button)findViewById(R.id.btnNext);
+        viewFlipper = (ViewFlipper)findViewById(R.id.viewFlipper);
+
+        btnPrev.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                viewFlipper.showPrevious();
+            }
+        });
+
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                viewFlipper.showNext();
+            }
+        });
     }
 }
