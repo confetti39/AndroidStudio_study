c8ac7a620ab89ddcce9155a4413bf034ada99bd8
“깃 <hy04730@naver.com>
2021-10-22 11:22:47 +0900

    ViewFlipper(3)-취소버튼제어

--- Project6_2/app/src/main/java/com/example/project6_2/MainActivity.java
+++ Project6_2/app/src/main/java/com/example/project6_2/MainActivity.java
@@ -3,17 +3,21 @@ package com.example.project6_2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 import android.widget.ViewFlipper;
 
 public class MainActivity extends AppCompatActivity {
     Button btnPrev, btnNext;
     ViewFlipper viewFlipper;
     float initX;
+    double initTime;
 
-    //이 부분에서 제너레이트 -> Override methods -> onTouchEvent
+    //터치제스처로 뷰플리퍼 다음화면, 이전화면
+    //이 부분에서 제너레이트 -> Override Methods -> onTouchEvent
     @Override
     public boolean onTouchEvent(MotionEvent event) {
         if(event.getAction() == MotionEvent.ACTION_DOWN) { //touch를 했다
@@ -25,6 +29,24 @@ public class MainActivity extends AppCompatActivity {
                 viewFlipper.showNext();
             } else if(diffX < -30){
                 viewFlipper.showPrevious();
+            }
+        }
+        return true;
+    }
+
+    //스마트폰 하단 back button 누르고 일정한 시간 내에 한 번 더 취소 누르면 취소 동작
+    //제너레이트 -> Override Methods -> onKeyDown
+    //keyCode=어떤 키가 눌렸는지 정보, keyEvent=이벤트 정보
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if(keyCode == KeyEvent.KEYCODE_BACK){
+            if(System.currentTimeMillis() - initTime > 3000){
+                //처음 취소버튼 누르면 currentTimeMillis에 엄청 큰 값이 저장되어 있고, initTime은 자동으로 0으로 초기화된 상태다.
+                Toast.makeText(MainActivity.this, "종료하려면 한 번 더 눌러주세요",
+                        Toast.LENGTH_SHORT).show();
+                initTime = System.currentTimeMillis();
+            } else {
+                finish(); //앱 종료
             }
         }
         return true;
