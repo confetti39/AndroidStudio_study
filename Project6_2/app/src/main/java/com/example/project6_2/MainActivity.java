b81892315d2b5f945d352c1717c4b2b3e2dbe87a
“깃 <hy04730@naver.com>
2021-10-22 11:11:57 +0900

    ViewFlipper(2)-터치제스처이동

--- Project6_2/app/src/main/java/com/example/project6_2/MainActivity.java
+++ Project6_2/app/src/main/java/com/example/project6_2/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.project6_2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ViewFlipper;
@@ -10,6 +11,24 @@ import android.widget.ViewFlipper;
 public class MainActivity extends AppCompatActivity {
     Button btnPrev, btnNext;
     ViewFlipper viewFlipper;
+    float initX;
+
+    //이 부분에서 제너레이트 -> Override methods -> onTouchEvent
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if(event.getAction() == MotionEvent.ACTION_DOWN) { //touch를 했다
+            initX = event.getRawX();
+        } else if(event.getAction() == MotionEvent.ACTION_UP) {
+            float diffX = initX - event.getRawX();
+
+            if(diffX > 30){
+                viewFlipper.showNext();
+            } else if(diffX < -30){
+                viewFlipper.showPrevious();
+            }
+        }
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,12 +45,13 @@ public class MainActivity extends AppCompatActivity {
                 viewFlipper.showPrevious();
             }
         });
-
         btnNext.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 viewFlipper.showNext();
             }
         });
+
+
     }
 }
